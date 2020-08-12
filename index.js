0a518c34511d968e7a10860cdc3d347fd55e9f15
Trojanekkk <W.Trojan27@gmail.com>
2020-08-12 17:04:13 +0200

    Edit: Set updateAll (cron) exec time to 01:30AM

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const CONF = JSON.parse(fs.readFileSync('./config.json'))
 const bot = new discord.Client()
 const token = CONF['token']
 const PREFIX = CONF['prefix']
-const task = cron.schedule('0 */24 * * *', () => updateAllUsers(false))
+const task = cron.schedule('0 1 * * *', () => updateAllUsers(false))
 task.start()
 
 
